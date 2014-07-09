5ae294afba9a3865729ac8c0d859f34d135e2225
xiafan68 <xiafan68@gmail.com>
2014-07-09 15:21:59 +0800

    argument generator factory

---
+++ src/edu/ecnu/imc/bsma/generator/GeneratorFactory.java
@@ -0,0 +1,16 @@
+package edu.ecnu.imc.bsma.generator;
+
+import java.util.List;
+
+import edu.ecnu.imc.bsma.util.Pair;
+
+public class GeneratorFactory {
+	public static Generator<List<String>> create(String type,
+			List<Pair<List<String>>> seeds) {
+		if (type.equals("uniform")){
+			
+		} else {
+			
+		}
+	}
+}
